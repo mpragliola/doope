842282b6c6d95cac69dbd5188ee2aefeb28330de
Marco Pragliola <marcopragliola@gmail.com>
2026-05-16 11:55:45 +0200

    feat(api): add mode param to autoMarkGroup

--- src/api.ts
+++ src/api.ts
@@ -21,8 +21,8 @@ export const api = {
   setFolderPriorities: (priorities: string[]) =>
     invoke<void>('set_folder_priorities', { priorities }),
 
-  autoMarkGroup: (groupId: string) =>
-    invoke<string[]>('auto_mark_group', { groupId }),
+  autoMarkGroup: (groupId: string, mode: 'priority' | 'quality') =>
+    invoke<string[]>('auto_mark_group', { groupId, mode }),
 
   deleteMarked: (pathsToDelete: string[]) =>
     invoke<string[]>('delete_marked', { pathsToDelete }),
